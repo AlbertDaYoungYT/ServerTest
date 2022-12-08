ffabf4219c93aa91e14c70cae4b87294e1770d13
AlbertDaYoungYT <kcobain465@gmail.com>
2022-12-08 10:25:24 +0100

    Signup

--- main.py
+++ main.py
@@ -37,11 +37,21 @@ def Shop():
 
 @app.route("/confirmsignin", methods=["POST"])
 def ConfirmLogin():
-    ID = uuid.uuid1()
-    session["id"] = ID
-    Data.CreateUser(ID, request.form["user"], hashlib.sha512(request.form["pwd"].encode()).hexdigest())
-    return redirect(url_for('HomePage'))
+    if Data.ValidateUser(request.form["user"], hashlib.sha512(request.form["pwd"].encode()).hexdigest()):
+        Uid = Data.FetchUID(request.form["user"], hashlib.sha512(request.form["pwd"].encode()).hexdigest())
+        if Uid != False:
+            ID = uuid.uuid1()
+            session["id"] = ID
+            session["Uid"] = Uid
+            return redirect(url_for('HomePage'))
+        else:
+            flash("Uid could not be found, please contact an admin")
+            return redirect(url_for("SignIn"))
 
+
+    flash("Incorrect Username or Password")
+    return redirect(url_for("SignIn"))
+    
 @app.route("/signin")
 def SignIn():
     return render_template("login.html")
@@ -50,6 +60,37 @@ def SignIn():
 def LogOut():
     session.clear()
     return redirect(url_for('HomePage'))
+
+
+@app.route("/confirmsignup", methods=["POST"])
+def ConfirmLogin():
+    if not Data.ValidateUser(request.form["user"], hashlib.sha512(request.form["pwd"].encode()).hexdigest()):
+        ID = uuid.uuid3()
+        Uid = uuid.uuid1()
+        session["id"] = ID
+        session["Uid"] = Uid
+        Data.CreateUser(Uid, request.form["dname"], request.form["user"], hashlib.sha512(request.form["pwd"].encode()).hexdigest())
+        # ----------HERE----------
+        if Uid != False:
+            return redirect(url_for('HomePage'))
+        else:
+            flash("Uid could not be found, please contact an admin")
+            return redirect(url_for("SignIn"))
+
+
+    flash("User already exists")
+    return redirect(url_for("SignIn"))
+
+@app.route("/signup")
+def SignUp():
+    return render_template("signup.html")
+
+
+"""@app.route("/profile/<Uid>")
+def ProfileURL(Uid):
+    if Data.ValidateID(Uid):
+"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- modules/DataBase.py
+++ modules/DataBase.py
@@ -1,9 +1,45 @@
 import db as DB
 import time
 
-def CreateUser(ID, Username, PwdHash):
+def CreateUser(ID, DisplayName, Username, PwdHash):
     cursor = DB.db.cursor()
     cursor.execute(f"INSERT INTO users (ID, Username, PwdHash, CreationDate) VALUES ('{ID}', '{Username}', '{PwdHash}', '{time.time()}')")
-    cursor.execute(f"INSERT INTO userdata (ID, DisplayName, ProfileIMG, isadmin) VALUES ('{ID}', '{Username}', 'default.png', 'False')")
+    cursor.execute(f"INSERT INTO userdata (ID, DisplayName, ProfileIMG, isadmin) VALUES ('{ID}', '{DisplayName}', 'default.png', 'False')")
 
     DB.db.commit()
+
+def ValidateUser(Username, PwdHash):
+    cursor = DB.db.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+
+    for result in results:
+        result = list(result)
+        if result[1] == Username and result[2] == PwdHash:
+            return True
+    
+    return False
+
+def ValidateID(ID):
+    cursor = DB.db.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+
+    for result in results:
+        result = list(result)
+        if result[0] == ID:
+            return True
+    
+    return False
+
+def FetchUID(Username, PwdHash):
+    cursor = DB.db.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+
+    for result in results:
+        result = list(result)
+        if result[1] == Username and result[2] == PwdHash:
+            return result[0]
+    
+    return False
