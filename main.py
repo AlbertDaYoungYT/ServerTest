aae7a0ff9adbfa89dbdc95a774edac683b987b0b
AlbertDaYoungYT <kcobain465@gmail.com>
2022-12-09 12:10:13 +0100

    Index retheme

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def HomePage():
     Uid = session.get('Uid')
     if Uid == None:
         Theme = T.FetchDefaultTheme()
-        return render_template('index.html', UserProfileSrc="Test", UserName="Test", UserProfile="Test", isloggedin=False, isadmin=False, color1=Theme[1], color2=Theme[2], color3=Theme[3], color4=Theme[4])
+        return render_template('index.html', UserProfileSrc="Test", UserName="Test", UserProfile="Test", isloggedin=False, isadmin=False, color1=Theme[1], color2=Theme[2], color3=Theme[3], color4=Theme[4], color5=Theme[5])
     else:
         data = list(UP.FetchUserdata(Uid))
         if ''.join(data) == "NOTFOUND":
-            raise("Error Reading Userdata, please contact admin")
+            return redirect(url_for("LogOut"))
 
         try: Theme = session["theme"]
         except Exception: Theme = "default"
@@ -38,7 +38,7 @@ def HomePage():
         else:
             admin = True
 
-        return render_template('index.html', UserProfileSrc=f"static/favicons/{data[2]}", UserName=data[1], UserProfile=f"{data[0]}", isloggedin=True, isadmin=admin, color1=Theme[1], color2=Theme[2], color3=Theme[3], color4=Theme[4])
+        return render_template('index.html', UserProfileSrc=f"static/favicons/{data[2]}", UserName=data[1], UserProfile=f"{data[0]}", isloggedin=True, isadmin=admin, color1=Theme[1], color2=Theme[2], color3=Theme[3], color4=Theme[4], color5=Theme[5])
 
 @app.route("/about")
 def About():
@@ -102,4 +102,4 @@ def ProfileURL(uid):
     return redirect(url_for("HomePage"))
 
 if __name__ == '__main__':
-    app.run(host="192.168.218.138", debug=True)
+    app.run(host="192.168.111.240", debug=True)
