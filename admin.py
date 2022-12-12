7ef9e2c8c137fb82ed57f1c61e85676c898eb90f
AlbertDaYoungYT <65977153+AlbertDaYoungYT@users.noreply.github.com>
2022-12-12 10:22:59 +0100

    Admin Init

---
+++ admin.py
@@ -0,0 +1,16 @@
+from __main__ import *
+
+import data.text as Text
+import modules.DataBase as Data
+import modules.UserProfile as UP
+import modules.Theme as T
+import data.settings as S
+import db as DB
+
+app.route("/admin")
+def Admin():
+    return redirect(url_for("AdminHome"))
+
+app.route("/admin/home")
+def AdminHome():
+    return render_template("admin/index")
